62fa17f570311d5f7e1f992cda31c6597ac9e074
Adonai Domingez <donihernandez003@gmail.com>
2023-06-09 13:29:00 -0400

    Initial Commit

---
+++ Contactify/ContactifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactifyApp.swift
+//  Contactify
+//
+//  Created by Doni on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
